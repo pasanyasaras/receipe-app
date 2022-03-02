cf722ed1695b6716a812500c4cdbb3c89d71da41
pasanyasaras <pasansmart@gmail.com>
2022-03-02 23:01:39 +0530

    initial commit

---
+++ src/test/java/com/pasanyasara/receipe/ReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasanyasara.receipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
