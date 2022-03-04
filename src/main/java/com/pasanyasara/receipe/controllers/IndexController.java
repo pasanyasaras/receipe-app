e71aeb80a71d4d2fc9cb510e2f75e16482fefcc8
pasanyasaras <pasansmart@gmail.com>
2022-03-04 22:20:02 +0530

    creating index page

---
+++ src/main/java/com/pasanyasara/receipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.pasanyasara.receipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getStringPage()
+    {
+        return "index";
+    }
+}
